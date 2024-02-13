1f3f74d6ede0b3ef07b1b588175559256e9c0ca4
aayushswkl <152261646+aayushswkl@users.noreply.github.com>
2024-02-13 20:11:09 +0545

    aayush_chaulagain_2417735.js

--- script.js
+++ script.js
@@ -1,82 +1,105 @@
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const apiKey ="fc29d269a2765dfcdb95c6f5b7370014"
+const apiUrl = "http://localhost/prototype2/aayush_chaulagain_2417735.php?q=";
 const searchBOx = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weatherIcon");
+const date = document.getElementById("date");
+const form = document.querySelector("form");
 
+window.addEventListener("load", () => {
+  // Check if online, fetch data from API, otherwise fetch from local storage
+  if (navigator.onLine) {
+    checkWeather("Brisbane");
+  } else {
+    const storedData = localStorage.getItem("Brisbane");
+    const data = JSON.parse(storedData);
 
-window.addEventListener("load", defaultCity())
-    async function defaultCity(){
-        const response = await fetch(apiUrl + "brisbane" + `&appid=${apiKey}`)
-    let data = await response.json();
-    
+    if (data) {
+      displayWeatherFromStorage(data);
+    } else {
+      alert("No data stored in local storage");
+    }
+  }
+});
+
+async function checkWeather(city) {
+  try {
+    const response = await fetch(`http://localhost/prototype2/aayush_chaulagain_2417735.php?q=${city}`);
+    const data = await response.json();
+
     console.log(data);
+ 
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp)+"°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "kmh";
+    document.getElementById("city").innerText = data.city;
+    document.getElementById("temperature").innerText = Math.round(data.temperature) + " °C";
+    document.getElementById("humidity").innerText = data.humidity + " %";
+    document.querySelector(".wind").innerText = data.wind_speed + " kmh";
+    document.getElementById("pressure").innerText = data.pressure + " pa";
 
-    
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src= "clouds.jpg";
-        document.querySelector(".description").innerHTML="cloudy";   
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src= "clear.jpg";
-        document.querySelector(".description").innerHTML="its a clear day";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src= "rain.jpg";
-        document.querySelector(".description").innerHTML="RAINNNN";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src= "drizzle.jpg";
-        document.querySelector(".description").innerHTML="Drizzly day";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src= "mist.jpg";
-        document.querySelector(".description").innerHTML="misty day";
-    }
-    }
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.iconcode}@2x.png`;
 
+    document.querySelector("#description").innerText = data.weather_condition;
+    date.innerText = data.day_date;
 
-async function checkWeather(city){
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
-    var data = await response.json();
-    
-    console.log(data);
+    localStorage.setItem(city, JSON.stringify(data));
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp)+"°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "kmh";
+  
+  } catch (error) {
+    alert(`something went wrong ${error}`);
+  }
+}
 
-    if(data.weather[0].main == "Clouds"){
-        weatherIcon.src= "clouds.jpg";
-        document.querySelector(".description").innerHTML="cloudy";   
-    }
-    else if(data.weather[0].main == "Clear"){
-        weatherIcon.src= "clear.jpg";
-        document.querySelector(".description").innerHTML="its a clear day";
-    }
-    else if(data.weather[0].main == "Rain"){
-        weatherIcon.src= "rain.jpg";
-        document.querySelector(".description").innerHTML="RAINNNN";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-        weatherIcon.src= "drizzle.jpg";
-        document.querySelector(".description").innerHTML="Drizzly day";
-    }
-    else if(data.weather[0].main == "Mist"){
-        weatherIcon.src= "mist.jpg";
-        document.querySelector(".description").innerHTML="misty day";
-    }
+function displayWeatherFromStorage(data) {
+  // Display weather data from local storage
+  document.getElementById("city").innerText = data.city;
+  document.getElementById("temperature").innerText =
+    Math.round(data.temperature) + " °C";
+  document.getElementById("humidity").innerText = data.humidity + " %";
+  document.querySelector(".wind").innerText = data.wind_speed + " kmh";
+  document.getElementById("pressure").innerText = data.pressure + " pa";
+
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.iconcode}@2x.png`;
+
+  document.querySelector("#description").innerText = data.weather_condition;
+  date.innerText = data.day_date;
 }
 
 
-searchButton.addEventListener("click", ()=>{
-    checkWeather(searchBOx.value);
+form.addEventListener("submit",(event)=>{
+  event.preventDefault();
+  checkWeather(searchBOx.value);
 })
 
 
+
+async function fetchLast7DaysWeather() {
+  try {
+    const response = await fetch('http://localhost/prototype2/aayush_chaulagain_2417735.php?last7days=true');
+    let data = await response.text(); 
+    const arrayData = data.substring(data.indexOf('['), data.lastIndexOf(']') + 1);
+    const weatherData = JSON.parse(arrayData);
+    displayWeatherData(weatherData);
+  } catch (error) {
+    console.error('Error fetching last 7 days weather:', error);
+  }
+}
+
+function displayWeatherData(weatherData) {
+
+  const weatherDataElement = document.getElementById('weatherData');
+  weatherDataElement.innerHTML = ''; 
+
+  weatherData.forEach(day => {
+    const dayElement = document.createElement('div');
+    dayElement.classList.add('weather-day');
+    dayElement.innerHTML = `
+      <div class="date">Date: ${day.day_date}</div>
+      <div class="temperature">Temperature: ${Math.round(day.temperature)}°C</div>
+      <div class="weather-icon">
+        <img src="https://openweathermap.org/img/wn/${day.iconcode}@2x.png" alt="Weather Icon">
+      </div>
+    `;
+    weatherDataElement.appendChild(dayElement);
+  });
+}
+
+fetchLast7DaysWeather();
